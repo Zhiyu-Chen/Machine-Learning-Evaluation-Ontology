5196e9ae124f12d4c4e6bd4d0360b375d990f372
medtray <med.trabelsi.ept@gmail.com>
2020-04-16 14:59:13 -0400

    update

---
+++ train.py
@@ -0,0 +1,84 @@
+from model import *
+from utils import *
+from evaluate import *
+import argparse
+parser = argparse.ArgumentParser(description='LSTM-CRF', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument("--data", type=str, default='data/train.csv',
+                    help="dataset to use")
+parser.add_argument('--epochs', type=int, default=100)
+
+parser.add_argument("--CharToIdx", type=str, default='data/train.char_to_idx',
+                    help="char_to_idx")
+parser.add_argument("--WordToIdx", type=str, default='data/train.word_to_idx',
+                    help="word_to_idx")
+parser.add_argument("--TagToIdx", type=str, default='data/train.tag_to_idx',
+                    help="tag_to_idx")
+parser.add_argument("--model_file", type=str, default='data/model',
+                     help="dataset to use")
+parser.add_argument("--valid", type=str, default='data/valid',
+                     help="dataset to use")
+
+args = parser.parse_args()
+
+def load_data(args):
+    data = dataloader()
+    batch = []
+    cti = load_tkn_to_idx(args.CharToIdx) # char_to_idx
+    wti = load_tkn_to_idx(args.WordToIdx) # word_to_idx
+    itt = load_idx_to_tkn(args.TagToIdx) # idx_to_tkn
+    print("loading %s..." % args.data)
+    with open(args.data, "r") as fo:
+        text = fo.read().strip().split("\n" * (HRE + 1))
+
+
+    for block in text:
+        for line in block.split("\n"):
+            x, y = line.split("\t")
+            x = [x.split(":") for x in x.split(" ")]
+            y = [int(y)] if HRE else [int(x) for x in y.split(" ")]
+            for xc,xw in x:
+                if len(xc.split("+"))==1:
+                    print('here')
+            xc, xw = zip(*[(list(map(int, xc.split("+"))), int(xw)) for xc, xw in x])
+            data.append_item(xc = [xc], xw = [xw], y0 = y)
+        data.append_row()
+    data.strip()
+    for _batch in data.split():
+        xc, xw = data.tensor(_batch.xc, _batch.xw, _batch.lens)
+        _, y0 = data.tensor(None, _batch.y0, sos = True)
+        batch.append((xc, xw, y0))
+    print("data size: %d" % len(data.y0))
+    print("batch size: %d" % BATCH_SIZE)
+    return batch, cti, wti, itt
+
+def train(args):
+    num_epochs = int(args.epochs)
+    batch, cti, wti, itt = load_data(args)
+    model = rnn_crf(len(cti), len(wti), len(itt))
+    optim = torch.optim.Adam(model.parameters(), lr = LEARNING_RATE)
+    print(model)
+    epoch = load_checkpoint(args.model_file, model) if isfile(args.model_file) else 0
+    filename = re.sub("\.epoch[0-9]+$", "", args.model_file)
+    print("training model...")
+    for ei in range(epoch + 1, epoch + num_epochs + 1):
+        loss_sum = 0
+        timer = time()
+        for xc, xw, y0 in batch:
+            loss = model(xc, xw, y0) # forward pass and compute loss
+            loss.backward() # compute gradients
+            optim.step() # update parameters
+            loss_sum += loss.item()
+        timer = time() - timer
+        loss_sum /= len(batch)
+        if ei % SAVE_EVERY and ei != epoch + num_epochs:
+            save_checkpoint("", None, ei, loss_sum, timer)
+        else:
+            save_checkpoint(filename, model, ei, loss_sum, timer)
+        if EVAL_EVERY and (ei % EVAL_EVERY == 0 or ei == epoch + num_epochs):
+            argss = [model, cti, wti, itt]
+            evaluate(predict(args.valid, *argss), True)
+            model.train()
+            print()
+
+train(args)
---
+++ utils.py
@@ -0,0 +1,204 @@
+import sys
+import re
+from time import time
+from os.path import isfile
+from parameters import *
+from collections import defaultdict
+
+def normalize(x):
+    # x = re.sub("[\uAC00-\uD7A3]+", "\uAC00", x) £ convert Hangeul to 가
+    # x = re.sub("[\u3040-\u30FF]+", "\u3042", x) # convert Hiragana and Katakana to あ
+    # x = re.sub("[\u4E00-\u9FFF]+", "\u6F22", x) # convert CJK unified ideographs to 漢
+    x = re.sub("\s+", " ", x)
+    x = re.sub("^ | $", "", x)
+    x = x.lower()
+    return x
+
+def tokenize(x, norm = True):
+    if norm:
+        x = normalize(x)
+    if UNIT == "char":
+        return re.sub(" ", "", x)
+    if UNIT in ("word", "sent"):
+        return x.split(" ")
+
+def save_data(filename, data):
+    fo = open(filename, "w")
+    for seq in data:
+        fo.write((" ".join(seq[0]) + "\t" + " ".join(seq[1]) if seq else "") + "\n")
+    fo.close()
+
+def load_tkn_to_idx(filename):
+    print("loading %s" % filename)
+    tkn_to_idx = {}
+    fo = open(filename)
+    for line in fo:
+        line = line[:-1]
+        tkn_to_idx[line] = len(tkn_to_idx)
+    fo.close()
+    return tkn_to_idx
+
+def load_idx_to_tkn(filename):
+    print("loading %s" % filename)
+    idx_to_tkn = []
+    fo = open(filename)
+    for line in fo:
+        line = line[:-1]
+        idx_to_tkn.append(line)
+    fo.close()
+    return idx_to_tkn
+
+def save_tkn_to_idx(filename, tkn_to_idx):
+    fo = open(filename, "w")
+    for tkn, _ in sorted(tkn_to_idx.items(), key = lambda x: x[1]):
+        fo.write("%s\n" % tkn)
+    fo.close()
+
+def load_checkpoint(filename, model = None):
+    print("loading %s" % filename)
+    checkpoint = torch.load(filename)
+    if model:
+        model.load_state_dict(checkpoint["state_dict"])
+    epoch = checkpoint["epoch"]
+    loss = checkpoint["loss"]
+    print("saved model: epoch = %d, loss = %f" % (checkpoint["epoch"], checkpoint["loss"]))
+    return epoch
+
+def save_checkpoint(filename, model, epoch, loss, time):
+    print("epoch = %d, loss = %f, time = %f" % (epoch, loss, time))
+    if filename and model:
+        print("saving %s" % filename)
+        checkpoint = {}
+        checkpoint["state_dict"] = model.state_dict()
+        checkpoint["epoch"] = epoch
+        checkpoint["loss"] = loss
+        torch.save(checkpoint, filename + ".epoch%d" % epoch)
+        print("saved model at epoch %d" % epoch)
+
+class dataloader():
+    def __init__(self):
+        for a, b in self.data().__dict__.items():
+            setattr(self, a, b)
+
+    class data():
+        def __init__(self):
+            self.idx = None # input index
+            self.x0 = [[]] # raw input
+            self.x1 = [[]] # tokenized input
+            self.xc = [[]] # indexed input, character-level
+            self.xw = [[]] # indexed input, word-level
+            self.y0 = [[]] # actual output
+            self.y1 = [] # predicted output
+            self.lens = None # document lengths
+            self.prob = [] # probability
+            self.attn = [] # attention heatmap
+
+    def append_item(self, x0 = None, x1 = None, xc = None, xw = None, y0 = None):
+        if x0: self.x0[-1].extend(x0)
+        if x1: self.x1[-1].extend(x1)
+        if xc: self.xc[-1].extend(xc)
+        if xw: self.xw[-1].extend(xw)
+        if y0: self.y0[-1].extend(y0)
+
+    def append_row(self):
+        self.x0.append([])
+        self.x1.append([])
+        self.xc.append([])
+        self.xw.append([])
+        self.y0.append([])
+
+    def strip(self):
+        if len(self.xw[-1]):
+            return
+        self.x0.pop()
+        self.x1.pop()
+        self.xc.pop()
+        self.xw.pop()
+        self.y0.pop()
+
+    def sort(self):
+        self.idx = list(range(len(self.x0)))
+        self.idx.sort(key = lambda x: -len(self.xw[x] if HRE else self.xw[x][0]))
+        self.x0 = [self.x0[i] for i in self.idx]
+        self.x1 = [self.x1[i] for i in self.idx]
+        self.xc = [self.xc[i] for i in self.idx]
+        self.xw = [self.xw[i] for i in self.idx]
+
+    def unsort(self):
+        self.idx = sorted(range(len(self.x0)), key = lambda x: self.idx[x])
+        self.x0 = [self.x0[i] for i in self.idx]
+        self.x1 = [self.x1[i] for i in self.idx]
+        self.xc = [self.xc[i] for i in self.idx]
+        self.xw = [self.xw[i] for i in self.idx]
+        self.y1 = [self.y1[i] for i in self.idx]
+        if self.prob: self.prob = [self.prob[i] for i in self.idx]
+        if self.attn: self.attn = [self.attn[i] for i in self.idx]
+
+    def split(self): # split into batches
+        for i in range(0, len(self.y0), BATCH_SIZE):
+            batch = self.data()
+            j = i + min(BATCH_SIZE, len(self.x0) - i)
+            batch.x0 = self.x0[i:j]
+            batch.y0 = self.y0[i:j]
+            batch.y1 = [[] for _ in range(j - i)]
+            batch.lens = [len(x) for x in self.xw[i:j]]
+            batch.prob = [Tensor([0]) for _ in range(j - i)]
+            batch.attn = [[] for _ in range(j - i)]
+            if HRE:
+                batch.x1 = [list(x) for x in self.x1[i:j] for x in x]
+                batch.xc = [list(x) for x in self.xc[i:j] for x in x]
+                batch.xw = [list(x) for x in self.xw[i:j] for x in x]
+            else:
+                batch.x1 = [list(*x) for x in self.x1[i:j]]
+                batch.xc = [list(*x) for x in self.xc[i:j]]
+                batch.xw = [list(*x) for x in self.xw[i:j]]
+            yield batch
+
+    def tensor(self, bc, bw, lens = None, sos = False, eos = False):
+        _p, _s, _e = [PAD_IDX], [SOS_IDX], [EOS_IDX]
+        if HRE and lens:
+            d_len = max(lens) # document length (Ld)
+            i, _bc, _bw = 0, [], []
+            for j in lens:
+                if sos:
+                    _bc.append([[]])
+                    _bw.append([])
+                _bc.extend(bc[i:i + j] + [[[]] for _ in range(d_len - j)])
+                _bw.extend(bw[i:i + j] + [[] for _ in range(d_len - j)])
+                if eos:
+                    _bc.append([[]])
+                    _bw.append([])
+                i += j
+            bc, bw = _bc, _bw
+        if bw:
+            s_len = max(map(len, bw)) # sentence length (Ls)
+            bw = [_s * sos + x + _e * eos + _p * (s_len - len(x)) for x in bw]
+            bw = LongTensor(bw) # [B * Ld, Ls]
+        if bc:
+            w_len = max(max(map(len, x)) for x in bc) # word length (Lw)
+            w_pad = [_p * (w_len + 2)]
+            bc = [[_s + w + _e + _p * (w_len - len(w)) for w in x] for x in bc]
+            bc = [w_pad * sos + x + w_pad * (s_len - len(x) + eos) for x in bc]
+            bc = LongTensor(bc) # [B * Ld, Ls, Lw]
+        return bc, bw
+
+def log_sum_exp(x):
+    m = torch.max(x, -1)[0]
+    return m + torch.log(torch.sum(torch.exp(x - m.unsqueeze(-1)), -1))
+
+def iob_to_txt(x, y): # for word/sentence segmentation
+    out = [[]]
+    if re.match("(\S+/\S+( |$))+", x): # token/tag
+        x = re.sub(r"/[^ /]+\b", "", x) # remove tags
+    for i, (j, k) in enumerate(zip(tokenize(x, False), y)):
+        if i and k[0] == "B":
+            out.append([])
+        out[-1].append(j)
+    if TASK == "word-segmentation":
+        d1, d2 = "", " "
+    if TASK == "sentence-segmentation":
+        d1, d2 = " ", "\n"
+    return d2.join(d1.join(x) for x in out)
+
+def f1(p, r):
+    return 2 * p * r / (p + r) if p + r else 0
