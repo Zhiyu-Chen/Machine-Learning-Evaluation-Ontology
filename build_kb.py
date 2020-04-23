aa949792f63b084aad1babb2ae8cff0a49ad4bbc
Zhiyu-Chen <chenzhiyu.aier@outlook.com>
2020-04-23 15:49:33 -0400

    kb

---
+++ build_kb.py
@@ -0,0 +1,22 @@
+from metadata import *
+from reader import PaperReader
+from owlready2 import *
+import os
+
+NLP_TDM_rs_path = os.path.join(NLP_TDM_path,'annotations','resultsAnnotation.tsv')
+
+
+preader = PaperReader()
+paper_result = preader.get_paper_result()
+
+class SDO(object):
+    def __init__(self,onto_fname):
+        onto_path.append(onto_path)
+        onto = get_ontology(os.path.join(onto_path,onto_fname))
+        onto.load()
+
+    def create_individual_from(self,result_file):
+        pass
+
+    def create_relation_from(self,result_file):
+        pass
--- metadata.py
+++ metadata.py
@@ -5,3 +5,5 @@ base_dir = '/home/colozoy/exp/sci_data/science-result-extractor'
 cat_NLP_TDM_path = os.path.join(base_dir,'data/exp/few-shot-setup','NLP-TDMS')
 
 NLP_TDM_path = os.path.join(base_dir,'data','NLP-TDMS')
+onto_path = './data/sdo'
+onto_fname = 'scientific_dataset.owx'
