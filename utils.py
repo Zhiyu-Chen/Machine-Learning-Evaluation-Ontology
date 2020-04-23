efd8247861cf491c0bc36da2c317d883a06af61b
Mohamed Trabelsi <med.trabelsi.ept@gmail.com>
2020-04-23 13:45:59 -0400

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,422 +1,204 @@
-import numpy as np
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-import string
-import tensorflow as tf
-import os
+import sys
 import re
+from time import time
+from os.path import isfile
+from parameters import *
+from collections import defaultdict
 
+def normalize(x):
+    # x = re.sub("[\uAC00-\uD7A3]+", "\uAC00", x) £ convert Hangeul to 가
+    # x = re.sub("[\u3040-\u30FF]+", "\u3042", x) # convert Hiragana and Katakana to あ
+    # x = re.sub("[\u4E00-\u9FFF]+", "\u6F22", x) # convert CJK unified ideographs to 漢
+    x = re.sub("\s+", " ", x)
+    x = re.sub("^ | $", "", x)
+    x = x.lower()
+    return x
 
-def isEnglish(s):
-    try:
-        s.encode(encoding='utf-8').decode('ascii')
-    except UnicodeDecodeError:
-        return False
-    else:
-        return True
+def tokenize(x, norm = True):
+    if norm:
+        x = normalize(x)
+    if UNIT == "char":
+        return re.sub(" ", "", x)
+    if UNIT in ("word", "sent"):
+        return x.split(" ")
 
-def camel_case_split(identifier):
-    matches = re.finditer('.+?(?:(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|$)', identifier)
-    return [m.group(0) for m in matches]
+def save_data(filename, data):
+    fo = open(filename, "w")
+    for seq in data:
+        fo.write((" ".join(seq[0]) + "\t" + " ".join(seq[1]) if seq else "") + "\n")
+    fo.close()
 
-def preprocess(input,type):
+def load_tkn_to_idx(filename):
+    print("loading %s" % filename)
+    tkn_to_idx = {}
+    fo = open(filename)
+    for line in fo:
+        line = line[:-1]
+        tkn_to_idx[line] = len(tkn_to_idx)
+    fo.close()
+    return tkn_to_idx
 
-    if type=='attribute':
-        w = input.replace('-', " ").replace('_', ' ').replace('/', ' ').replace(',', ' ').replace('.', ' ').replace('|', ' ').replace(':', ' ')
-        #w = input.replace('_', ' ')
+def load_idx_to_tkn(filename):
+    print("loading %s" % filename)
+    idx_to_tkn = []
+    fo = open(filename)
+    for line in fo:
+        line = line[:-1]
+        idx_to_tkn.append(line)
+    fo.close()
+    return idx_to_tkn
 
-        tokens = word_tokenize(w)
+def save_tkn_to_idx(filename, tkn_to_idx):
+    fo = open(filename, "w")
+    for tkn, _ in sorted(tkn_to_idx.items(), key = lambda x: x[1]):
+        fo.write("%s\n" % tkn)
+    fo.close()
 
-        camel_tokens=[]
+def load_checkpoint(filename, model = None):
+    print("loading %s" % filename)
+    checkpoint = torch.load(filename)
+    if model:
+        model.load_state_dict(checkpoint["state_dict"])
+    epoch = checkpoint["epoch"]
+    loss = checkpoint["loss"]
+    print("saved model: epoch = %d, loss = %f" % (checkpoint["epoch"], checkpoint["loss"]))
+    return epoch
 
-        for w in tokens:
-            inter = camel_case_split(w)
-            camel_tokens += inter
+def save_checkpoint(filename, model, epoch, loss, time):
+    print("epoch = %d, loss = %f, time = %f" % (epoch, loss, time))
+    if filename and model:
+        print("saving %s" % filename)
+        checkpoint = {}
+        checkpoint["state_dict"] = model.state_dict()
+        checkpoint["epoch"] = epoch
+        checkpoint["loss"] = loss
+        torch.save(checkpoint, filename + ".epoch%d" % epoch)
+        print("saved model at epoch %d" % epoch)
 
-        tokens=camel_tokens
+class dataloader():
+    def __init__(self):
+        for a, b in self.data().__dict__.items():
+            setattr(self, a, b)
 
-        # convert to lower case
-        tokens = [w.lower() for w in tokens]
+    class data():
+        def __init__(self):
+            self.idx = None # input index
+            self.x0 = [[]] # raw input
+            self.x1 = [[]] # tokenized input
+            self.xc = [[]] # indexed input, character-level
+            self.xw = [[]] # indexed input, word-level
+            self.y0 = [[]] # actual output
+            self.y1 = [] # predicted output
+            self.lens = None # document lengths
+            self.prob = [] # probability
+            self.attn = [] # attention heatmap
 
-        inter_words = []
-        for w in tokens:
-            inter = re.sub(r'\u2013+', ' ', w).split()
-            inter_words += inter
+    def append_item(self, x0 = None, x1 = None, xc = None, xw = None, y0 = None):
+        if x0: self.x0[-1].extend(x0)
+        if x1: self.x1[-1].extend(x1)
+        if xc: self.xc[-1].extend(xc)
+        if xw: self.xw[-1].extend(xw)
+        if y0: self.y0[-1].extend(y0)
 
-        inter_words2 = []
-        for w in inter_words:
-            inter = re.sub(r'\u2014+', ' ', w).split()
-            inter_words2 += inter
-        # remove punctuation from each word
-        # table = str.maketrans('', '', string.punctuation)
-        # stripped = [w.translate(table) for w in tokens]
-        # remove remaining tokens that are not alphabetic
-        words = [word for word in inter_words2 if word.isalpha()]
-        # filter out stop words
-        stop_words = set(stopwords.words('english'))
-        words = [w for w in words if not w in stop_words]
+    def append_row(self):
+        self.x0.append([])
+        self.x1.append([])
+        self.xc.append([])
+        self.xw.append([])
+        self.y0.append([])
 
-        # final_words = []
-        # for w in words:
-        #     inter = re.sub('([a-z])([A-Z])', r'\1 \2', w).split()
-        #     final_words += inter
+    def strip(self):
+        if len(self.xw[-1]):
+            return
+        self.x0.pop()
+        self.x1.pop()
+        self.xc.pop()
+        self.xw.pop()
+        self.y0.pop()
 
-        final_words=words
+    def sort(self):
+        self.idx = list(range(len(self.x0)))
+        self.idx.sort(key = lambda x: -len(self.xw[x] if HRE else self.xw[x][0]))
+        self.x0 = [self.x0[i] for i in self.idx]
+        self.x1 = [self.x1[i] for i in self.idx]
+        self.xc = [self.xc[i] for i in self.idx]
+        self.xw = [self.xw[i] for i in self.idx]
 
-        final_words = [tok for tok in final_words if isEnglish(tok)]
+    def unsort(self):
+        self.idx = sorted(range(len(self.x0)), key = lambda x: self.idx[x])
+        self.x0 = [self.x0[i] for i in self.idx]
+        self.x1 = [self.x1[i] for i in self.idx]
+        self.xc = [self.xc[i] for i in self.idx]
+        self.xw = [self.xw[i] for i in self.idx]
+        self.y1 = [self.y1[i] for i in self.idx]
+        if self.prob: self.prob = [self.prob[i] for i in self.idx]
+        if self.attn: self.attn = [self.attn[i] for i in self.idx]
 
-    elif type=='value':
-        #w = input.replace('_', ' ').replace(',', ' ')
-        w = input.replace('-', " ").replace('_', ' ').replace('/', ' ').replace(',', ' ').replace('.', ' ').replace('|', ' ').replace(':', ' ')
-        #w=input
-
-        tokens = word_tokenize(w)
-
-        camel_tokens = []
-
-        for w in tokens:
-            inter = camel_case_split(w)
-            camel_tokens += inter
-
-        tokens = camel_tokens
-
-        # convert to lower case
-        tokens = [w.lower() for w in tokens]
-        inter_words = []
-        for w in tokens:
-            inter = re.sub(r'\u2013+', ' ', w).split()
-            inter_words += inter
-
-        inter_words2 = []
-        for w in inter_words:
-            inter = re.sub(r'\u2014+', ' ', w).split()
-            inter_words2 += inter
-
-        # remove punctuation from each word
-        # table = str.maketrans('', '', string.punctuation)
-        # stripped = [w.translate(table) for w in tokens]
-        # remove remaining tokens that are not alphabetic
-
-        numerical_values=[]
-        string_values=[]
-        for word in inter_words2:
-            try:
-                float(word)
-                numerical_values.append(word)
-
-            except ValueError:
-                string_values.append(word)
-
-
-        string_values_final=[]
-        for w in string_values:
-            inter=re.split(r'(\d+)', w)
-
-            for word in inter:
-                if len(word)>0:
-                    try:
-                        float(word)
-                        numerical_values.append(word)
-
-                    except ValueError:
-                        string_values_final.append(word)
-
-        #keep 0 digits
-        #numerical_values = [re.sub('\d', '#', s) for s in numerical_values]
-
-        #keep 1 digit
-        numerical_values_inter=[]
-        for s in numerical_values:
-            if s[0]=='-':
-                ss=s[2::]
-                ss=re.sub('\d', '#', ss)
-                ss=s[0:2]+ss
-
-
+    def split(self): # split into batches
+        for i in range(0, len(self.y0), BATCH_SIZE):
+            batch = self.data()
+            j = i + min(BATCH_SIZE, len(self.x0) - i)
+            batch.x0 = self.x0[i:j]
+            batch.y0 = self.y0[i:j]
+            batch.y1 = [[] for _ in range(j - i)]
+            batch.lens = [len(x) for x in self.xw[i:j]]
+            batch.prob = [Tensor([0]) for _ in range(j - i)]
+            batch.attn = [[] for _ in range(j - i)]
+            if HRE:
+                batch.x1 = [list(x) for x in self.x1[i:j] for x in x]
+                batch.xc = [list(x) for x in self.xc[i:j] for x in x]
+                batch.xw = [list(x) for x in self.xw[i:j] for x in x]
             else:
-                ss = s[1::]
-                ss = re.sub('\d', '#', ss)
-                ss = s[0] + ss
-
-            numerical_values_inter += [ss]
-
-        #keep 2 digits
-
-        # for s in numerical_values:
-        #     ss=s[2::]
-        #     ss=re.sub('\d', '#', ss)
-        #     ss=s[0:2]+ss
-        #     numerical_values_inter+=[ss]
-
-        numerical_values=numerical_values_inter
-        inter_words2 = string_values_final
-
-        words = [word for word in inter_words2 if word.isalpha() or word in['$','@','%','£','€','°']]
-        # filter out stop words
-        stop_words = set(stopwords.words('english'))
-        stop_words.remove('d')
-        stop_words.remove('m')
-        stop_words.remove('s')
-
-        words = [w for w in words if not w in stop_words]
-
-        # final_words = []
-        # for w in words:
-        #     inter = re.sub('([a-z])([A-Z])', r'\1 \2', w).split()
-        #     final_words += inter
-
-        final_words=words
-
-        final_words = [tok for tok in final_words if isEnglish(tok) or tok in['$','@','%','£','€','°']]
-
-        final_words=final_words+numerical_values
-
-
-
-    elif type=='value2':
-
-        w = input.replace('-', " ").replace('_', ' ').replace('/', ' ').replace(',', ' ').replace('.', ' ').replace('|', ' ').replace(':', ' ')
-
-        tokens = word_tokenize(w)
-
-        # convert to lower case
-        tokens = [w.lower() for w in tokens]
-        inter_words = []
-        for w in tokens:
-            inter = re.sub(r'\u2013+', ' ', w).split()
-            inter_words += inter
-
-        inter_words2 = []
-        for w in inter_words:
-            inter = re.sub(r'\u2014+', ' ', w).split()
-            inter_words2 += inter
-
-
-        numerical_values=[]
-        string_values=[]
-        for word in inter_words2:
-            try:
-                float(word)
-                numerical_values.append(word)
-
-            except ValueError:
-                string_values.append(word)
-
-
-        string_values_final=[]
-        for w in string_values:
-            inter=re.split(r'(\d+)', w)
-
-            for word in inter:
-                if len(word)>0:
-                    try:
-                        float(word)
-                        numerical_values.append(word)
-
-                    except ValueError:
-                        string_values_final.append(word)
-
-
-
-        inter_words2 = string_values_final
-
-        words = [word for word in inter_words2 if word.isalpha() or word in['$','@','%','£','€','°']]
-        # filter out stop words
-        stop_words = set(stopwords.words('english'))
-        stop_words.remove('d')
-        stop_words.remove('m')
-        stop_words.remove('s')
-
-        words = [w for w in words if not w in stop_words]
-
-        final_words = []
-        for w in words:
-            inter = re.sub('([a-z])([A-Z])', r'\1 \2', w).split()
-            final_words += inter
-
-        final_words = [tok for tok in final_words if isEnglish(tok) or tok in['$','@','%','£','€','°']]
-
-        final_words=final_words+numerical_values
-
-
-
-
-
-
-    elif type == 'description':
-        #w = input.replace('_', ' ').replace(',', ' ').replace('-', " ").replace('.', ' ')
-        w = input.replace('-', " ").replace('_', ' ').replace('/', ' ').replace(',', ' ').replace('.', ' ').replace('|', ' ').replace(':', ' ')
-
-
-        tokens = word_tokenize(w)
-
-        camel_tokens = []
-
-        for w in tokens:
-            inter = camel_case_split(w)
-            camel_tokens += inter
-
-        tokens = camel_tokens
-
-        # convert to lower case
-        tokens = [w.lower() for w in tokens]
-        inter_words = []
-        for w in tokens:
-            inter = re.sub(r'\u2013+', ' ', w).split()
-            inter_words += inter
-
-        inter_words2 = []
-        for w in inter_words:
-            inter = re.sub(r'\u2014+', ' ', w).split()
-            inter_words2 += inter
-        # remove punctuation from each word
-        #table = str.maketrans('', '', string.punctuation)
-        #stripped = [w.translate(table) for w in tokens]
-        # remove remaining tokens that are not alphabetic
-        words = [word for word in inter_words2 if word.isalpha()]
-        # filter out stop words
-        stop_words = set(stopwords.words('english'))
-        words = [w for w in words if not w in stop_words]
-
-        # final_words=[]
-        # for w in words:
-        #     inter=re.sub('([a-z])([A-Z])', r'\1 \2', w).split()
-        #     final_words+=inter
-
-        final_words=words
-
-        final_words = [tok for tok in final_words if isEnglish(tok)]
-
-        #not_to_use=['com','u','comma','separated','values','csv','data','dataset','https','api','www','http','non','gov','rows','p','download','downloads','file','files','p']
-
-        #final_words=[tok for tok in final_words if tok not in not_to_use]
-
-    return final_words
-
-
-
-def find_chunk_and_sample(dir,batch_size,nb_steps_per_epoch):
-    CHECKPOINT_DIR = dir + '/skip_gram_model_with_context'
-    checkpoint = tf.train.get_checkpoint_state(CHECKPOINT_DIR)
-
-    total_size=0
-
-    with tf.Session() as sess:
-
-
-        saver = tf.train.import_meta_graph(checkpoint.model_checkpoint_path + '.meta')
-        saver.restore(sess, checkpoint.model_checkpoint_path)
-        # aa=[n.name for n in tf.get_default_graph().as_graph_def().node]
-        bb = [v for v in tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)]
-
-        glob_step = bb[4]
-        glob_step = sess.run(glob_step)
-
-        nb_steps_last_epoch =glob_step%nb_steps_per_epoch
-
-        nb_samples_processed=int(nb_steps_last_epoch*batch_size)
-
-
-
-        input_folder = dir + '/X'
-
-        nb_chunks = len(os.listdir(input_folder))
-
-        for chunk in range(nb_chunks):
-            train_x_file = 'x_train_' + str(chunk + 1) + '.npy'
-
-            train_x_file = os.path.join(input_folder, train_x_file)
-
-            x_train = np.load(train_x_file)
-            total_size+=len(x_train)
-
-            if total_size>nb_samples_processed:
-                break
-
-
-        current_chunk=chunk+1
-
-        current_sample=len(x_train)-(total_size-nb_samples_processed)
-
-    return current_chunk,current_sample
-
-
-def steps_per_epoch(dir,batch_size):
-    total_size=0
-    input_folder = dir + '/X'
-
-    nb_chunks = len(os.listdir(input_folder))
-
-    for chunk in range(nb_chunks):
-        train_x_file = 'x_train_' + str(chunk + 1) + '.npy'
-
-        train_x_file = os.path.join(input_folder, train_x_file)
-
-        x_train = np.load(train_x_file)
-        total_size += len(x_train)
-
-
-    nb_steps_per_epoch=total_size//batch_size
-
-    rest_of_samples=total_size%batch_size
-
-    if rest_of_samples>0:
-        nb_steps_per_epoch+=1
-
-    return nb_steps_per_epoch
-
-
-
-def dcg_score(y_true, y_score, k=10, gains="exponential"):
-    """Discounted cumulative gain (DCG) at rank k
-    Parameters
-    ----------
-    y_true : array-like, shape = [n_samples]
-        Ground truth (true relevance labels).
-    y_score : array-like, shape = [n_samples]
-        Predicted scores.
-    k : int
-        Rank.
-    gains : str
-        Whether gains should be "exponential" (default) or "linear".
-    Returns
-    -------
-    DCG @k : float
-    """
-    order = np.argsort(y_score)[::-1]
-    y_true = np.take(y_true, order[:k])
-
-    if gains == "exponential":
-        gains = 2 ** y_true - 1
-    elif gains == "linear":
-        gains = y_true
-    else:
-        raise ValueError("Invalid gains option.")
-
-    # highest rank is 1 so +2 instead of +1
-    discounts = np.log2(np.arange(len(y_true)) + 2)
-    return np.sum(gains / discounts)
-
-
-def ndcg_score(y_true, y_score, k=10, gains="exponential"):
-    """Normalized discounted cumulative gain (NDCG) at rank k
-    Parameters
-    ----------
-    y_true : array-like, shape = [n_samples]
-        Ground truth (true relevance labels).
-    y_score : array-like, shape = [n_samples]
-        Predicted scores.
-    k : int
-        Rank.
-    gains : str
-        Whether gains should be "exponential" (default) or "linear".
-    Returns
-    -------
-    NDCG @k : float
-    """
-    best = dcg_score(y_true, y_true, k, gains)
-    actual = dcg_score(y_true, y_score, k, gains)
-    return actual / best
+                batch.x1 = [list(*x) for x in self.x1[i:j]]
+                batch.xc = [list(*x) for x in self.xc[i:j]]
+                batch.xw = [list(*x) for x in self.xw[i:j]]
+            yield batch
+
+    def tensor(self, bc, bw, lens = None, sos = False, eos = False):
+        _p, _s, _e = [PAD_IDX], [SOS_IDX], [EOS_IDX]
+        if HRE and lens:
+            d_len = max(lens) # document length (Ld)
+            i, _bc, _bw = 0, [], []
+            for j in lens:
+                if sos:
+                    _bc.append([[]])
+                    _bw.append([])
+                _bc.extend(bc[i:i + j] + [[[]] for _ in range(d_len - j)])
+                _bw.extend(bw[i:i + j] + [[] for _ in range(d_len - j)])
+                if eos:
+                    _bc.append([[]])
+                    _bw.append([])
+                i += j
+            bc, bw = _bc, _bw
+        if bw:
+            s_len = max(map(len, bw)) # sentence length (Ls)
+            bw = [_s * sos + x + _e * eos + _p * (s_len - len(x)) for x in bw]
+            bw = LongTensor(bw) # [B * Ld, Ls]
+        if bc:
+            w_len = max(max(map(len, x)) for x in bc) # word length (Lw)
+            w_pad = [_p * (w_len + 2)]
+            bc = [[_s + w + _e + _p * (w_len - len(w)) for w in x] for x in bc]
+            bc = [w_pad * sos + x + w_pad * (s_len - len(x) + eos) for x in bc]
+            bc = LongTensor(bc) # [B * Ld, Ls, Lw]
+        return bc, bw
+
+def log_sum_exp(x):
+    m = torch.max(x, -1)[0]
+    return m + torch.log(torch.sum(torch.exp(x - m.unsqueeze(-1)), -1))
+
+def iob_to_txt(x, y): # for word/sentence segmentation
+    out = [[]]
+    if re.match("(\S+/\S+( |$))+", x): # token/tag
+        x = re.sub(r"/[^ /]+\b", "", x) # remove tags
+    for i, (j, k) in enumerate(zip(tokenize(x, False), y)):
+        if i and k[0] == "B":
+            out.append([])
+        out[-1].append(j)
+    if TASK == "word-segmentation":
+        d1, d2 = "", " "
+    if TASK == "sentence-segmentation":
+        d1, d2 = " ", "\n"
+    return d2.join(d1.join(x) for x in out)
 
 def f1(p, r):
     return 2 * p * r / (p + r) if p + r else 0
