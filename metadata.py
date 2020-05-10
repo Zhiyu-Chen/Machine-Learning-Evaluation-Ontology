556258969a197f55b25c3d4e4aaa0aaba7f08ea6
Zhiyu Chen <Zhiyu-Chen@users.noreply.github.com>
2020-05-10 01:04:13 -0400

    Update metadata.py

--- metadata.py
+++ metadata.py
@@ -3,7 +3,7 @@ import os
 base_dir = './'  #dir to the acl paper data
 proj_dir = './' #dir to the project
 
-cat_NLP_TDM_path = os.path.join(base_dir,'data/exp/few-shot-setup','NLP-TDMS')
+cat_NLP_TDM_path = os.path.join(base_dir,'data/NLP-TDMS')
 kb_dir = os.path.join(base_dir,'data/NLP-TDMS/kb')
 datasetKB_path = os.path.join(kb_dir,'datasetKB.tsv')
 metricKB_path = os.path.join(kb_dir,'evaMatrixKB.tsv')
